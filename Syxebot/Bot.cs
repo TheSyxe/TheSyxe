67def533d13077ba349eaadb0966374b66afb6fd
Weston Thomas <thesyxe@comcast.net>
2015-01-09 18:02:09 -0600

    major refactor to switchcases

--- Syxebot/Bot.cs
+++ Syxebot/Bot.cs
@@ -22,10 +22,12 @@ namespace Syxebot.Syxebot
         private Dictionary<string, string> _commands;
         private IrcClient _client;
         private Dictionary<string, int> _cmdFlood;
+        //string identifier, string message, int seconds
+        private Dictionary<string, KeyValuePair<string, UInt64>> _timedMessages;
         private bool _isConnected = false;
         private System.Timers.Timer _timer;
         private string path;
-
+        private UInt64 _ticks;
 
         #endregion
 
@@ -65,10 +67,15 @@ namespace Syxebot.Syxebot
             _subscribers = new List<string>();
             _commands = new Dictionary<string, string>();
             _cmdFlood = new Dictionary<string, int>();
+            _timedMessages = new Dictionary<string, KeyValuePair<string, UInt64>>();
             _timer = new System.Timers.Timer(1000);
             _timer.AutoReset = true;
+            _ticks = 0;
             _timer.Elapsed += (s, e) =>
             {
+                _ticks++;
+                //log("ticks" + _ticks.ToString());
+                TimedMessageTimer();
                 //TODO: Add timed messages and points (spawn new thread probably or Threading.Timer)
                 FloodTimer();
             };
@@ -88,17 +95,16 @@ namespace Syxebot.Syxebot
         }
         #endregion
 
+        #region Private Methods
 
         private void MainThread()
         {
-
-
             #region Subscribed Events
 
             #region bot.ChannelMessageRecieved
             _client.ChannelMessageRecieved += (s, e) =>
             {
-                parseChannelMessage(new ChatMessage(_client.Channels[e.PrivateMessage.Source], e));
+                parseMessage(new ChatMessage(_client.Channels[e.PrivateMessage.Source], e));
             };
 
             #endregion
@@ -177,200 +183,246 @@ namespace Syxebot.Syxebot
 
             _client.ConnectAsync();
             while (true) { }
-
-        }
-        #region Private Methods
-
-        private void getSubscribers()
-        {
-            var client = new RestClient();
-            client.BaseUrl = new Uri("https://api.twitch.tv/kraken");
-            client.Authenticator = new SimpleAuthenticator("ClientID", _ircConfig.ClientID, "ClientSecret", _ircConfig.ClientSecret);
-
         }
 
-        private void parseChannelMessage(ChatMessage cm)
+        private void doParseUserMessage(ChatMessage cm)
         {
-            #region Jtv Message
-            if (cm.Nick.ToLower() == "jtv")
+            #region Display Logic
+            string prefix = "";
+            if (isOp(_moderators, cm.Nick))
+                prefix += "@";
+            if (_subscribers.Contains(cm.Nick))
+                prefix += "(Sub)";
+            log(prefix + cm.Nick + ": " + cm.Message);
+            #endregion
+
+            switch (cm.MessageParams[0])
             {
+                case "!cmd":
+                case "!command":
+                    doParseCommand(cm);
+                    break;
 
-                #region SpecialUser
-                //twitch turbo, staff, or subscriber
-                if (cm.Message.ToLower().StartsWith("specialuser"))
-                {
-                    #region Channel Subscriber
-                    if (cm.MessageParams[2].ToLower() == "subscriber")
+                case "!timed":
+                case "!timer":
+                    doParseTimerCommand(cm);
+                    break;
+
+                default:
+                    #region Softcoded or no command
+                    try
                     {
-                        if (!_subscribers.Contains(cm.MessageParams[1]))
-                            _subscribers.Add(cm.MessageParams[1]);
+                        string val = "";
+                        if (_commands.TryGetValue(cm.MessageParams[0], out val))
+                        {
+
+                            if (!_cmdFlood.ContainsKey(cm.MessageParams[0].ToLower()))
+                            {
+                                _client.SendAction(val, cm.Channel.Name);
+                                _cmdFlood.Add(cm.MessageParams[0].ToLower(), _ircConfig.commandCooldown);
+
+                            }
+                        }
+                    }
+                    catch
+                    {
+
                     }
                     #endregion
-                }
-                #endregion
-
-                #region UserColor
-                else if (cm.Message.ToLower().StartsWith("usercolor"))
-                {
-
-                }
-                #endregion
-
-                #region EmoteSet
-                else if (cm.Message.ToLower().StartsWith("emoteset"))
-                {
-
-                }
-                #endregion
-
-                #region ModeratorList
-                if (cm.Message.StartsWith("The moderators of this room are: "))
-                {
-                    //remove "The moderators of this room are: "
-                    string trim = cm.Message.Remove(0, 33);
-                    //split list by ", " into array
-                    string[] mods = trim.Split(new string[] { ",", " " }, StringSplitOptions.RemoveEmptyEntries);
-
-                    //put into the channelModerators list
-                    foreach (var mod in mods)
-                    {
-                        if (!_moderators.Contains(mod))
-                            _moderators.Add(mod);
-                    }
-
-                }
-                #endregion
+                    break;
             }
-            #endregion
+        }
 
-            #region Twitch Notify Message
-            else if (cm.Nick.ToLower() == "twitchnotify")
+        private void doParseCommand(ChatMessage cm)
+        {
+            //TODO: add userlevel switch
+            if (cm.Identifiers >= 3 && (cm.MessageParams[1].ToLower() == "add" || cm.MessageParams[1].ToLower() == "edit"))
             {
-                #region Subscription Notification
-                if (cm.Message.EndsWith("subscribed!"))
+                if (!_commands.ContainsKey(cm.MessageParams[1].ToLower()))
                 {
-                    if (_subscribers.Contains(cm.MessageParams[0]))
-                        _subscribers.Add(cm.MessageParams[0]);
-                    Console.WriteLine("{0} has just subscribed!", cm.MessageParams[0]);
-                }
-                #endregion
-            }
-            #endregion
-
-            #region User Message
-            else
-            {
-                #region Display Logic
-                string prefix = "";
-                if (isOp(_moderators, cm.Nick))
-                    prefix += "@";
-                if (_subscribers.Contains(cm.Nick))
-                    prefix += "(Sub)";
-                log(prefix + cm.Nick + ": " + cm.Message);
-                #endregion
-
-                #region Bot Logic
-
-                #region Custom Commands
-
-                try
-                {
-                    string val = "";
-                    if (_commands.TryGetValue(cm.MessageParams[0], out val))
+                    string value = "";
+                    for (int i = 2; i < cm.Identifiers; i++)
                     {
-
-                        if (!_cmdFlood.ContainsKey(cm.MessageParams[0].ToLower()))
-                        {
-                            _client.SendAction(val, _ircConfig.channel);
-                            _cmdFlood.Add(cm.MessageParams[0].ToLower(), _ircConfig.commandCooldown);
-
-                        }
+                        value += cm.MessageParams[i];
+                        value += " ";
                     }
-                }
-                catch
-                {
-
-                }
-
-                #region !addcom
-                //check if enough identifiers are passed to be valid
-                //TODO: add userlevel switch
-                if (cm.MessageParams[0].ToLower() == "!addcom" && cm.Identifiers >= 2)
-                {
-                    //if command doesnt exist
-                    if (!_commands.ContainsKey(cm.MessageParams[1].ToLower()))
+                    if (cm.MessageParams[1].ToLower() == "add")
                     {
-                        string value = "";
-                        for (int i = 2; i < cm.Identifiers; i++)
-                        {
-                            value += cm.MessageParams[i];
-                            value += " ";
-                        }
                         _commands.Add(cm.MessageParams[1].ToLower(), value);
-                        _client.SendAction(cm.MessageParams[1].ToLower() + " command added.", _ircConfig.channel);
-                    }
-
-                }
-                #endregion
-
-                #region !delcom
-                if (cm.MessageParams[0].ToLower() == "!delcom" && cm.Identifiers >= 1)
-                {
-                    if (_commands.ContainsKey(cm.MessageParams[1].ToLower()))
-                    {
-                        _commands.Remove(cm.MessageParams[1]);
-                        _client.SendAction(cm.MessageParams[1].ToLower() + "command deleted.", _ircConfig.channel);
+                        _client.SendAction(cm.MessageParams[1].ToLower() + " command added.", cm.Channel.Name);
                     }
                     else
                     {
-                        _client.SendAction(cm.Nick + ", " + cm.MessageParams[1].ToLower() + " command not found.", _ircConfig.channel);
-                    }
-                }
-                #endregion
-
-                #region !editcom
-                if (cm.MessageParams[0].ToLower() == "!editcom" && cm.Identifiers >= 2)
-                {
-                    if (_commands.ContainsKey(cm.MessageParams[1].ToLower()))
-                    {
-                        string value = "";
-                        for (int i = 2; i < cm.Identifiers; i++)
-                        {
-                            value += cm.MessageParams[i];
-                            value += " ";
-                        }
                         _commands[cm.MessageParams[1].ToLower()] = value;
-                        _client.SendAction(cm.MessageParams[1].ToLower() + " command updated.", _ircConfig.channel);
+                        _client.SendAction(cm.MessageParams[1].ToLower() + " command updated.", cm.Channel.Name);
                     }
-
                 }
-                #endregion
-
-                #endregion
-
-                #region Timed Messages
-
-                //!timer add 3600 steam Come join my steam group!
-                if (cm.Message.ToLower().StartsWith("!timer add") && cm.Identifiers >= 4)
+                else
                 {
-                    int minutes;
-                    bool parseResult = Int32.TryParse(cm.MessageParams[2], out minutes);
-                    if (!parseResult)
+                    _client.SendAction(cm.Nick + ", " + cm.MessageParams[1].ToLower() + " command not found.", cm.Channel.Name);
+                }
+            }
+            else if (cm.MessageParams[1].ToLower() == "del" || cm.MessageParams[1].ToLower() == "rem" || cm.MessageParams[1].ToLower() == "remove" || cm.MessageParams[1].ToLower() == "delete")
+            {
+                if (_commands.ContainsKey(cm.MessageParams[1].ToLower()))
+                {
+                    _commands.Remove(cm.MessageParams[1]);
+                    _client.SendAction(cm.MessageParams[1].ToLower() + "command deleted.", cm.Channel.Name);
+                }
+                else
+                {
+                    _client.SendAction(cm.Nick + ", " + cm.MessageParams[1].ToLower() + " command not found.", cm.Channel.Name);
+                }
+            }
+        }
+
+
+        private void doParseTimerCommand(ChatMessage cm)
+        {
+            //!timer add 3600 steam Come join my steam group!
+            if (cm.MessageParams[1].ToLower() == "add" && cm.Identifiers >= 4)
+            {
+                UInt64 minutes;
+                bool parseResult = UInt64.TryParse(cm.MessageParams[2], out minutes);
+                if (!parseResult)
+                {
+                    _client.SendAction("Invalid parameter.", cm.Channel.Name);
+                }
+                else if (!_timedMessages.ContainsKey(cm.MessageParams[3]))
+                {
+                    string timedMessage = "";
+                    minutes = minutes * 60;
+                    for (int i = 4; i < cm.Identifiers; i++)
                     {
-                        _client.SendAction("Invalid parameter.", IrcConfig.channel);
+                        timedMessage += cm.MessageParams[i];
+                        timedMessage += " ";
                     }
-                    else
-                    {
-                        //TODO: continue work here...
-                    }
+                    _timedMessages.Add(cm.MessageParams[3].ToLower(), new KeyValuePair<string, UInt64>(timedMessage, minutes));
+
+                    //TODO: better message to send
+                    _client.SendAction("Timed message has been added", cm.Channel.Name);
+                }
+                else
+                {
+                    //TODO: better message to send
+                    _client.SendAction("Already exists.", cm.Channel.Name);
+                }
+            }
+                //!timer delete steam
+            else if (cm.MessageParams[1].ToLower() == "del" || cm.MessageParams[1].ToLower() == "rem" || cm.MessageParams[1].ToLower() == "remove" || cm.MessageParams[1].ToLower() == "delete")
+            {
+                //TODO: implement logig
+            }
+                //!timer stop steam
+            else if (cm.MessageParams[1].ToLower() == "stop")
+            {
+                //TODO: implement logig
+            }
+                //!timer start steam
+            else if (cm.MessageParams[1].ToLower() == "start")
+            {
+                //TODO: implement logig
+            }
+        }
+
+
+
+        #region Name Parse Methods
+        //Find out the name of who sent it
+        private void parseMessage(ChatMessage cm)
+        {
+            switch (cm.Nick.ToLower())
+            {
+                case "jtv":
+                    doParseJtvMessage(cm);
+                    break;
+                case "twitchnotify":
+                    doParseTwitchNotifyMessage(cm);
+                    break;
+                default:
+                    doParseUserMessage(cm);
+                    break;
+            }
+        }
+        //twitchnotify sent it
+        private void doParseTwitchNotifyMessage(ChatMessage cm)
+        {
+            #region Subscription Notification
+            if (cm.Message.EndsWith("subscribed!"))
+            {
+                if (_subscribers.Contains(cm.MessageParams[0]))
+                    _subscribers.Add(cm.MessageParams[0]);
+                //_client.SendAction(cm.MessageParams[0] + " has just subscribed!", cm.Channel.Name);
+                log(cm.MessageParams[0] + " has just subscribed!");
+            }
+            #endregion
+        }
+        //jtv sent it
+        private void doParseJtvMessage(ChatMessage cm)
+        {
+            #region Jtv Message
+
+            #region SpecialUser
+            //twitch turbo, staff, or subscriber
+            if (cm.Message.ToLower().StartsWith("specialuser"))
+            {
+                #region Channel Subscriber
+                if (cm.MessageParams[2].ToLower() == "subscriber")
+                {
+                    if (!_subscribers.Contains(cm.MessageParams[1]))
+                        _subscribers.Add(cm.MessageParams[1]);
                 }
                 #endregion
+            }
+            #endregion
 
-                #endregion
+            #region UserColor
+            else if (cm.Message.ToLower().StartsWith("usercolor"))
+            {
 
             }
             #endregion
 
+            #region EmoteSet
+            else if (cm.Message.ToLower().StartsWith("emoteset"))
+            {
+
+            }
+            #endregion
+
+            #region ModeratorList
+            if (cm.Message.StartsWith("The moderators of this room are: "))
+            {
+                //remove "The moderators of this room are: "
+                string trim = cm.Message.Remove(0, 33);
+                //split list by ", " into array
+                string[] mods = trim.Split(new string[] { ",", " " }, StringSplitOptions.RemoveEmptyEntries);
+
+                //put into the channelModerators list
+                foreach (var mod in mods)
+                {
+                    if (!_moderators.Contains(mod))
+                        _moderators.Add(mod);
+                }
+
+            }
+            #endregion
+
+            #endregion
+        }
+        #endregion
+
+        private void TimedMessageTimer()
+        {
+            foreach (var timedmsg in _timedMessages)
+            {
+                var r = _ticks % timedmsg.Value.Value;
+                if (r == 0)
+                {
+                    _client.SendAction(timedmsg.Value.Key, _ircConfig.channel);
+                    log("timed message fired");
+                }
+            }
         }
 
         private void FloodTimer()
@@ -384,6 +436,14 @@ namespace Syxebot.Syxebot
                 }
             }
             _cmdFlood = newDict;
+        }
+
+        private void getSubscribers()
+        {
+            var client = new RestClient();
+            client.BaseUrl = new Uri("https://api.twitch.tv/kraken");
+            client.Authenticator = new SimpleAuthenticator("ClientID", _ircConfig.ClientID, "ClientSecret", _ircConfig.ClientSecret);
+
         }
 
         #endregion
